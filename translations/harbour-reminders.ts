3b56e0fe40863d57c7d766d87f040a64fb0d5be8
François KUBLER <francois+github@kubler.org>
2015-10-15 16:17:24 +0200

    Updated translation file.

--- translations/harbour-reminders.ts
+++ translations/harbour-reminders.ts
@@ -9,6 +9,25 @@
     </message>
 </context>
 <context>
+    <name>DateTimePicker</name>
+    <message>
+        <source>Not set</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unset due date (and due time)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unset due date</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unset due time</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>FolderDelegate</name>
     <message>
         <source>Rename</source>
@@ -18,19 +37,22 @@
 <context>
     <name>Folders</name>
     <message>
-        <source>Smart Folders</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Projects</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <source>Reminders</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Add a Reminder...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Helpers</name>
+    <message>
+        <source>Smart Folders</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Projects</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -55,6 +77,294 @@
     </message>
     <message>
         <source>Trash</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Pending</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Recurring</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Waiting</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Deleted</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>None</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Low</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Medium</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>High</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Tomorrow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Yesterday</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ProjectPicker</name>
+    <message>
+        <source>Project</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Project name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>(new)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No project</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Reminder</name>
+    <message>
+        <source>Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Task</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Status</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Started</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Completed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Waiting</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Priority</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Project</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Due for</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Created</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Last modified</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Annotations</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Tags</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ReminderEditor</name>
+    <message>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Priority</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No priority</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Low</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Medium</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>High</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Project</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No project</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Due date</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Due time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Wait date</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Repeat</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The reminder will remain hidden until this date.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Remove wait date</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Removing wait date</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Not set</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source></source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Reminder description</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TagPicker</name>
+    <message>
+        <source>pending</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>completed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>deleted</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>overdue</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>due</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>today</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>yesterday</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>tomorrow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>week</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>month</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>year</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>until</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>active</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>scheduled</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>tagged</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>annotated</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>waiting</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>parent</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>child</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>blocked</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>blocking</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>ready</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
